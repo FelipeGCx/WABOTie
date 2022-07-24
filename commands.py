f46c764b1e2f52cb91f36b6d88dbc3a29f724d8d
FelipeGCx <ggfelipegc@gmail.com>
2022-07-24 14:52:00 -0500

    adding cmds btns

--- commands.py
+++ commands.py
@@ -15,7 +15,7 @@ def get_cmd_key(cmd):
 def get_cmds():
     cmds = []
     for i in commands:
-        string = f"\n ♦ ```{i['keywords'][0]}```"
+        string = f"\n 📌 ```{i['keywords'][0]}```"
         cmds.append(string)
     return ' '.join(cmds)
 
