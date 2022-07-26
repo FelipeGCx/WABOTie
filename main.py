59f6867be33cddc2152cdf01eca35c6e6f067a66
FelipeGCx <ggfelipegc@gmail.com>
2022-07-25 21:52:07 -0500

    testing response btn

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def webhook():
         print('########### THE CHANGES ############',request.get_json())
         changes = request_data['entry'][0]['changes'][0]
         if (request_data['object'] == 'whatsapp_business_account') and (changes['field'] == 'messages'):
-            sender_phone = changes['value']['contacts'][0]['wa_id']
+            sender_phone = changes['value']['messages'][0]['from']
             if changes['messages'][0].type == 'text':
                 # Get the message
                 message = changes['value']['messages'][0]['text']['body']
